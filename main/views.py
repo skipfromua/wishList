4da8c07570dfd80ddf79c04465b8e6300c12682a
Oleksandr <skipfromua@gmail.com>
2020-09-14 23:13:40 +0300

    to release

--- main/views.py
+++ main/views.py
@@ -7,10 +7,29 @@ from wishlist.models import Wishlist, WishObject
 
 
 def main(request):
+    context = {}
     if request.method == 'POST':
         needed_object = WishObject.objects.filter(id=int(request.POST['wishObject'])) & \
                         WishObject.objects.filter(wishlistId=int(request.POST['wishlist_from']))
-        needed_object.update(wishlistId=int(request.POST['wishlist_to']))
+        if needed_object:
+            print(request.user.id)
+            print(Wishlist.objects.filter(id=int(request.POST['wishlist_to'])).values('wishlistOwnerID'))
+            if request.user.id == Wishlist.objects.filter(
+                    id=int(request.POST['wishlist_to'])).values('wishlistOwnerID')[0]['wishlistOwnerID']:
+                my_new_object = WishObject.objects.create(
+                    objectName=needed_object.values('objectName')[0]['objectName'],
+                    objectDescription=needed_object.values('objectDescription')[0]['objectDescription'],
+                    reasonToHave=needed_object.values('reasonToHave')[0]['reasonToHave'],
+                    objectLocation=needed_object.values('objectLocation')[0]['objectLocation'],
+                    requirementsToDo=needed_object.values('requirementsToDo')[0]['requirementsToDo'],
+                    wishlistId=Wishlist.objects.get(id=int(request.POST['wishlist_to']))
+                )
+                my_new_object.save()
+                context.update({'status_msg': 'You successfully added object into one of your wishlists'})
+            else:
+                context.update({'status_msg': 'You cant add object into wishlists which is don`t belong to you =('})
+        else:
+            context.update({'status_msg': 'Cannot find specified object =('})
 
     user = get_user(request)
     try:
@@ -18,15 +37,19 @@ def main(request):
         wishlists = Wishlist.objects.filter(wishlistOwnerID=user_id)
         shared_wishlists = Wishlist.objects.filter(isSharedToOthers=True) & \
                            Wishlist.objects.exclude(wishlistOwnerID=user_id)
-        context = {
-            'username': user,
-            'user_id': user_id,
-            'your_wishlists': wishlists,
-            'shared_wishlists': shared_wishlists,
-        }
+        context.update(
+            {
+                'username': user,
+                'user_id': user_id,
+                'your_wishlists': wishlists,
+                'shared_wishlists': shared_wishlists,
+            }
+        )
         return render(request, 'main/main.html', context)
     except ObjectDoesNotExist:
-        context = {
-            'message': 'To use api you must login first'
-        }
+        context.update(
+            {
+                'message': 'To use api you must login first'
+            }
+        )
         return render(request, 'main/main.html', context)
